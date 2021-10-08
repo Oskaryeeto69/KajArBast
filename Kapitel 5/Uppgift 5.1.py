defd730a311c370b73a33be97265620b6628ca86
Oskaryeeto69 <oskar.forsgren@elev.ga.ntig.se>
2021-10-08 13:18:38 +0200

    apa

---
+++ Kapitel 5/Uppgift 5.1.py
@@ -0,0 +1,2 @@
+t1 ="Detta är en svart kung \u265a och detta är pi symbolen \u03C0"
+print (t1)
---
+++ Kapitel 5/Uppgift 5.2.py
@@ -0,0 +1,5 @@
+t1=input(str("Skriv vad du vill: "))
+if t1[0] == t1[-1]:
+    print("Första och sista bokstaven är lika")
+else:
+    print("Första och sista bokstaven är olika")
