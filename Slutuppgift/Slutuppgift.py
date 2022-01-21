86ffc641928605621e2e9b085b8710074f9843bb
Oskaryeeto69 <oskar.forsgren@elev.ga.ntig.se>
2022-01-21 13:22:08 +0000

    abc

--- Slutuppgift/Slutuppgift.py
+++ Slutuppgift/Slutuppgift.py
@@ -1,6 +1,6 @@
-import csv
+
 t1 = input("Detta program kommer visa det kändsate spelararen baserat på vilket nummer de har\nVilket tröjnummer mellan 1-99 vill du se: ")
-with open ("abc.csv") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(t1.join(row))
+
+from lista123 import list_of_words
+word = t1
+print(word)
