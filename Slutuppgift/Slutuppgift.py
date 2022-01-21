487e906cf9a2ccb688389ab2902fd026e7b62ef2
Oskaryeeto69 <oskar.forsgren@elev.ga.ntig.se>
2022-01-21 13:39:35 +0000

    adbd

--- Slutuppgift/Slutuppgift.py
+++ Slutuppgift/Slutuppgift.py
@@ -1,6 +1,6 @@
 
 t1 = input("Detta program kommer visa det kändsate spelararen baserat på vilket nummer de har\nVilket tröjnummer mellan 1-99 vill du se: ")
 
-from lista123 import list_of_words
-word = t1
-print(word)
+from lista123 import lista1
+spelare = t1(lista1)
+print(lista1)
