71f730aefe9b2414650ce8baf0807c69ffdc0a6a
Oskaryeeto69 <oskar.forsgren@elev.ga.ntig.se>
2022-02-18 10:23:12 +0000

    vasdasasdjk
    lasdis

--- Slutuppgift/Slutuppgift.py
+++
@@ -1,7 +0,0 @@
-
-t1 = input("Detta program kommer visa det kändsate spelararen baserat på vilket nummer de har\nVilket tröjnummer mellan 1-99 vill du se: ")
-it1 = int(t1)
-from lista123 import lista1
-spelare = (lista1[it1-1])
-
-print(spelare)
